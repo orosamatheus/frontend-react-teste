434c4edc679797a58c0baed329aa13a4a06ab018
Matheus Rosa <matheusrosa.la@gmail.com>
2022-06-02 15:31:36 -0300

    feat: add react hook form on form component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -9,17 +9,37 @@ import {
   NumberDecrementStepper,
   Button,
 } from "@chakra-ui/react";
-
 import { FormProps } from "./types";
+import { useContext } from "react";
+import { Context } from "../../contexts/fact";
 
 export default function Form({ isListOfFacts }: FormProps) {
+  const {
+    handleSubmit,
+    onSubmitFact,
+    onSubmitListOfFacts,
+    register,
+    isSubmitting,
+  } = useContext(Context);
+
+
+  const submitFunction = 
+    isListOfFacts
+      ? handleSubmit(onSubmitListOfFacts)
+      : handleSubmit(onSubmitFact);
+
   return (
-    <form>
+    <form onSubmit={submitFunction}>
       <Flex gap="5" justify="center" align="center">
         <FormControl>
-          <FormLabel>Tamanho do fato: </FormLabel>
-          <NumberInput min={0}>
-            <NumberInputField />
+          <FormLabel htmlFor="maxLength">Tamanho do fato: </FormLabel>
+          <NumberInput>
+            <NumberInputField
+              min={0}
+              name="maxLength"
+              id="maxLength"
+              {...register("maxLength")}
+            />
             <NumberInputStepper>
               <NumberIncrementStepper />
               <NumberDecrementStepper />
@@ -28,9 +48,14 @@ export default function Form({ isListOfFacts }: FormProps) {
         </FormControl>
         {isListOfFacts ? (
           <FormControl>
-            <FormLabel>Quantidade de fatos: </FormLabel>
-            <NumberInput min={0}>
-              <NumberInputField />
+            <FormLabel htmlFor="numberOfFacts">Quantidade de fatos: </FormLabel>
+            <NumberInput>
+              <NumberInputField
+                min={0}
+                id="numberOfFacts"
+                name="numberOfFacts"
+                {...register("numberOfFacts")}
+              />
               <NumberInputStepper>
                 <NumberIncrementStepper />
                 <NumberDecrementStepper />
@@ -40,7 +65,14 @@ export default function Form({ isListOfFacts }: FormProps) {
         ) : null}
       </Flex>
       <Flex justify="center">
-        <Button colorScheme='twitter' variant="solid" type="submit" mt={4} size="lg">
+        <Button
+          colorScheme="twitter"
+          variant="solid"
+          type="submit"
+          mt={4}
+          size="lg"
+          isLoading={isSubmitting}
+        >
           Buscar
         </Button>
       </Flex>
